81b42ef7ab30758127bb9d2f19e4e9b98e0e1369
igorolszewski <igorolszewski@op.pl>
2018-09-08 12:25:05 +0200

    angular app
    angular app

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*
+ * IIFE to keep code clean and safe
+ */
+(function(){
+
+    /*
+     * Declaration of main angular module for this appllication.
+     *
+     * It is named planetFacts and has no dependencies (hence the 
+     * empty array as the second argument)
+     */
+    angular
+        .module("planetFacts", []);
+
+})();
