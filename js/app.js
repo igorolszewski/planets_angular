fdab194af3bb3999fb57d4266a4bc2f9b3abef46
Igor Olszewski <igorolszewski@op.pl>
2018-10-26 22:33:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
-/*
- * IIFE to keep code clean and safe
- */
+
 (function(){
 
-    /*
-     * Declaration of main angular module for this appllication.
-     *
-     * It is named planetFacts and has no dependencies (hence the 
-     * empty array as the second argument)
-     */
     angular
         .module("planetFacts", []);
 
